424759c85e87cb8beef066cf03008294adb48b3a
vivi <41368076+veevyo@users.noreply.github.com>
2022-10-07 22:42:11 -0500

    extended distraction.rb

--- distraction.rb
+++ distraction.rb
@@ -19,7 +19,7 @@ live_loop :bass do
     wait 0.5
   end
 end
-wait 4
+wait 8
 live_loop :drums do
   with_fx :krush do
     sample :drum_heavy_kick
@@ -44,33 +44,35 @@ live_loop :melody do
   play :g3
   wait 0.5
 end
-wait 8
+wait 16
 live_loop :chords do
   use_synth :mod_pulse
-  2.times do
-    play :g4, amp: 0.3
-    play :b4, amp: 0.3
-    play :d5, amp: 0.3
-    wait 1
-  end
-  2.times do
-    play :e3, amp: 0.3
-    play :g3, amp: 0.3
-    play :b4, amp: 0.3
-    wait 1
-  end
-  2.times do
-    play :f3, amp: 0.3
-    play :a3, amp: 0.3
+  with_fx :reverb do
+    2.times do
+      play :g4, amp: 0.3
+      play :b4, amp: 0.3
+      play :d5, amp: 0.3
+      wait 1
+    end
+    2.times do
+      play :e3, amp: 0.3
+      play :g3, amp: 0.3
+      play :b4, amp: 0.3
+      wait 1
+    end
+    2.times do
+      play :f3, amp: 0.3
+      play :a3, amp: 0.3
+      play :c4, amp: 0.3
+      wait 1
+    end
+    play :a4, amp: 0.3
     play :c4, amp: 0.3
+    play :e4, amp: 0.3
+    wait 1
+    play :c4, amp: 0.3
+    play :e4, amp: 0.3
+    play :g4, amp: 0.3
     wait 1
   end
-  play :a4, amp: 0.3
-  play :c4, amp: 0.3
-  play :e4, amp: 0.3
-  wait 1
-  play :c4, amp: 0.3
-  play :e4, amp: 0.3
-  play :g4, amp: 0.3
-  wait 1
 end
